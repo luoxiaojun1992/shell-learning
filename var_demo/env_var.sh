5c595144ffb76725b1587c36929e80570b02c4d8
罗晓俊 <luoxiaojun1992@sina.cn>
2017-08-07 19:18:49 +0800

    add var

---
+++ var_demo/env_var.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo 'SHELL:' $SHELL
+echo 'BASH:' $BASH
+echo 'BASH_VERSION' $BASH_VERSION
+echo 'CDPATH:' $CDPATH
+echo 'EUID:' $EUID
+
+test_func()
+{
+	echo 'FUNCNAME:' $FUNCNAME
+}
+
+test_func
--- var_demo/local_var.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo $SHELL
-echo $BASH
-echo $BASH_VERSION

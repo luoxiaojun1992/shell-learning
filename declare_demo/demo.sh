e712af13ab1d79bb387f4026929eec3706aa26ac
罗晓俊 <luoxiaojun1992@sina.cn>
2017-08-07 17:13:39 +0800

    add declare demo

---
+++ declare_demo/demo.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+declare -i a=1
+echo $a
+
+declare -r b=2 
+
+declare -a arr='([0]=1 [1]=2)'
+echo $arr[0]
+echo $arr[1]
+
+test_func()
+{
+    return 1
+}
+
+declare -f test_func
+declare -F test_func

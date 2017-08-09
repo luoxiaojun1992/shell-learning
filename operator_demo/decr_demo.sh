bcffa64ae440d7cb3f5a5f18df2319d7e0cada5c
Gogs <gogitservice@gmail.com>
2017-08-09 21:47:44 +0800

    add incr & decr

---
+++ operator_demo/decr_demo.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+I=10
+let "J=--I"
+echo $J
+
+K=10
+let "L=K--"
+echo $K
---
+++ operator_demo/incr_demo.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+I=10
+let "J=++I"
+echo $J
+
+# 书上有，用不了
+# K=10
+# let "L=(K++)"
+# echo $L

b8abcc1d847c2aaafa056fbcaa6400386a139809
Gogs <gogitservice@gmail.com>
2017-08-14 21:20:53 +0800

    update monitor

--- tools/servers/monitor.sh
+++ tools/servers/monitor.sh
@@ -7,13 +7,13 @@ fi
 
 while :
 do
-ping $host -c 3
-timestamp=$(date +%Y-%m-%d\ %H:%M:%S)
-if [ $? -eq 0 ]; then
-    echo "$timestamp:Status On"
-else
-    echo "$timestamp:Status Off"
-fi
-sleep 5
+    ping $host -c 3
+    timestamp=$(date +%Y-%m-%d\ %H:%M:%S)
+    if [ $? -eq 0 ]; then
+        echo "$timestamp:Status On"
+    else
+        echo "$timestamp:Status Off"
+    fi
+    sleep 5
 done
 

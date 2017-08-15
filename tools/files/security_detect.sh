01c3a9896230aee93362d0d3eef50aa254bf2153
Gogs <gogitservice@gmail.com>
2017-08-15 21:26:40 +0800

    update security detect

--- tools/files/security_detect.sh
+++ tools/files/security_detect.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
 
+DIRS="/bin /usr/bin"
 
+FIND=/usr/bin/find
+
+MD5SUM=/usr/bin/md5sum
+
+MD5FILE=md5sum.md5
+
+$FIND $DIRS -type f | while read line
+do
+    $MD5SUM $line >> $MD5FILE
+done
 

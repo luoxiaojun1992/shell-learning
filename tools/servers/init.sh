89d434818e18ae246829ec55b4b80175c688ac55
Gogs <gogitservice@gmail.com>
2017-08-22 23:51:45 +0800

    update init

--- tools/servers/init.sh
+++ tools/servers/init.sh
@@ -1,3 +1,51 @@
 #! /bin/bash
 
+# yum            Update notifiction
 
+# Author:        Xiaojun Luo
+
+# chkconfig:     345 97 03
+
+# description:   Update yum packages
+
+# processname:   yum-updatesd
+
+# config:        /etc/yum/yum-updatesd.conf
+
+# pidfile:       /var/run/yum-updatesd.pid
+
+### BEGIN INIT INFO
+
+# Provides: yum-updatesd
+
+# Required-Start: $syslog $local_fs messagebus
+
+# Required-Stop: $syslog $local_fs messagebus
+
+# Default-Start: 2 3 4 5
+
+# Default-Stop: 0 1 6
+
+# Short-Description: Update Daemon
+
+# Description: Update Daemon
+
+### END INIT INFO
+
+. /etc/rc.d/init.d/functions
+
+RETVAL=0
+
+start() {
+
+}
+
+stop() {
+
+}
+
+restart() {
+
+}
+
+exit $RETVAL

4532d8a4e67210a123d126c0759058cf7048ee93
罗晓俊 <luoxiaojun1992@sina.cn>
2017-09-04 11:13:49 +0800

    update lnmp

--- tools/servers/lnmp.sh
+++ tools/servers/lnmp.sh
@@ -266,6 +266,12 @@ if [ $? -ne 0 ]; then
 	return 1
 fi
 
+sed -i '1,$s/;request_slowlog_timeout = 0/request_slowlog_timeout = 1s/g' /usr/local/etc/php-fpm.d/www.conf
+if [ $? -ne 0 ]; then
+	echo 'Failed to optimize php-fpm.conf'
+	return 1
+fi
+
 /usr/local/sbin/php-fpm -y /usr/local/etc/php-fpm.conf -c /usr/local/lib/php.ini
 if [ $? -ne 0 ]; then
 	echo 'Failed to start php-fpm'

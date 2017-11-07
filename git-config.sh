e21db97f951ab647e907ed5c76d2460185e11f4b
Gogs <gogitservice@gmail.com>
2017-11-07 17:45:55 +0800

    add git configuration script

---
+++ git-config.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+git config --global alias.ct commit
+git config --global alias.co checkout
+git config --global alias.pl pull
+git config --global alias.ps push
+git config --global alias.br branch
+git config --global alias.st status
+git config --global alias.ad add

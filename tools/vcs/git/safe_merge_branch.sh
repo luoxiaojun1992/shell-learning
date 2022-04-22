5f933aa0cf6d234f02b65b58a864607e685ed28a
luoxiaojun <xiao-jun.luo@hp.com>
2022-04-22 20:59:54 +0800

    feat: update safe_merge_branch.sh

--- tools/vcs/git/safe_merge_branch.sh
+++ tools/vcs/git/safe_merge_branch.sh
@@ -12,12 +12,15 @@ echo "Updateing $currentBranch"
 git pull origin $currentBranch
 
 echo "Creating backup branch"
-backupBranchExisted=`git branch | grep "$currentBranch-bak" | wc -l`
-if [ $backupBranchExisted -gt 0 ]; then
-	echo 'Backup branch existed, please confirm!'
+backupBranchCnt=`git branch | grep "$currentBranch-bak" | wc -l`
+newBackupBranchNum=$(($backupBranchCnt + 1))
+newBackupBranchName="$currentBranch-bak-$newBackupBranchNum"
+newBackBranchCnt=`git branch | grep $newBackupBranchName | wc -l`
+if [ $newBackBranchCnt -gt 0 ]; then
+	echo "New backup branch existed, please confirm!"
 	exit 1
 fi
-git branch "$currentBranch-bak"
+git branch "$newBackupBranchName"
 
 echo "Merging branch"
 git pull origin $sourceBranch --no-ff

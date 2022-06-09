99509e9e97bb6c237775b438317ddef901bcc581
luoxiaojun <xiao-jun.luo@hp.com>
2022-06-09 10:11:25 +0800

    feat: update tools/vcs/git/safe_co_branch.sh

--- tools/vcs/git/safe_co_branch.sh
+++ tools/vcs/git/safe_co_branch.sh
@@ -2,8 +2,18 @@
 
 originBr=$1
 echo "Origin branch:${originBr}"
+originBrCnt=`git branch | egrep "\\s+${originBr}$" | wc -l`
+if [ $originBrCnt -ne 1 ]; then
+	echo "Origin branch not existed"
+	exit 1
+fi
 
 newBr=$2
 echo "Target branch:${newBr}"
+newBrCnt=`git branch | egrep "\\s+${newBr}$" | wc -l`
+if [ $newBrCnt -ne 0 ]; then
+        echo "New branch existed"
+        exit 1
+fi
 
-git checkout ${originBr} && git checkout -b ${newBr}
+git checkout ${originBr} && git pull origin ${originBr} && git checkout -b ${newBr}

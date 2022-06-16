a51374b6a02faf79db63f762a4141de25128bee1
luoxiaojun <xiao-jun.luo@hp.com>
2022-06-16 14:21:27 +0800

    feat: update alias.sh

--- tools/alias.sh
+++ tools/alias.sh
@@ -2,6 +2,7 @@
 
 alias gpbranch='bash $HOME/shell/safe_merge_branch.sh `pwd`'
 alias gpmaster='bash $HOME/shell/safe_merge_branch.sh `pwd` master'
+alias gcbranch='bash $HOME/shell/safe_co_branch.sh'
 alias gcmaster='bash $HOME/shell/safe_co_branch.sh master'
 alias mgotidy='go version && make tidy'
 alias gotidy='go version && go mod tidy'
